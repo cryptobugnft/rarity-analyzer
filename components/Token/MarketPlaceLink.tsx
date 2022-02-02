5dd4dd74b586991b4f6f4e098c48f3307312dbb0
arachnidsNFT <98694860+arachnidsNFT@users.noreply.github.com>
2022-02-02 18:28:43 +0800

    marketplace link index

--- components/Token/MarketPlaceLink.tsx
+++ components/Token/MarketPlaceLink.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import Image from "next/image";
 import { OpenSeaIcon } from "../Icons";
 
-const MarketPlaceLinks = ({ id }: { id: string }) => {
+const MarketPlaceLinks = ({ id}: { id: string }) => {
+	id = id + 1;
 	return (
 		<div className="flex items-center justify-star space-x-1 mt-3 mb-5">
 			{process.env.NEXT_PUBLIC_COLLECTION_TOKEN && (
@@ -11,8 +12,7 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
 						href={`https://oasis.cash/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}/${id}`}
-					
-						target="_blank"
+						target="_blank" 
 						rel="noopener noreferrer nofollow"
 					>
 						<div className="flex items-center">
