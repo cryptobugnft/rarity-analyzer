2b0bcb36d78866fbd13af33e3a2db71eaf4d53bc
arachnidsNFT <98694860+arachnidsNFT@users.noreply.github.com>
2022-02-02 17:21:02 +0800

    update external links

--- components/Token/MarketPlaceLink.tsx
+++ components/Token/MarketPlaceLink.tsx
@@ -7,42 +7,32 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 		<div className="flex items-center justify-star space-x-1 mt-3 mb-5">
 			{process.env.NEXT_PUBLIC_COLLECTION_TOKEN && (
 				<>
+
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://opensea.io/assets/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}/${id}`}
+						href={`https://oasis.cash/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}/${id}`}
+					
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
 						<div className="flex items-center">
 							<span className="mr-2">
-								<OpenSeaIcon />
+								<Image src="/oasis_logo.svg" width="20px" height="20px" />
 							</span>
 							OpenSea
 						</div>
 					</a>
+
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://rarible.com/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}:${id}`}
-						target="_blank"
-						rel="noopener noreferrer nofollow"
-					>
-						<div className="flex items-center">
-							<span className="mr-2">
-								<Image src="/rarible.png" width="20px" height="20px" />
-							</span>
-							Rarible
-						</div>
-					</a>
-					<a
-						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://etherscan.io/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}?a=${id}`}
+						href={`https://www.smartscan.cash/address/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}?a=${id}`}
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
 						<div className="flex items-center">
 							<span className="mr-2">
 								<Image
-									src="/etherscan-logo-circle.png"
+									src="/SMARTBCH.png"
 									width="20px"
 									height="20px"
 								/>
