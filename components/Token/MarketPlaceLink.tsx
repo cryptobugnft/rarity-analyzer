b8bc3ed0bab7cc0ef132fae6df9dd7de2d4e4c66
cryptogesic <cryptobugnft_admin@protonmail.com>
2022-02-04 10:38:51 +0800

    update collection data

--- components/Token/MarketPlaceLink.tsx
+++ components/Token/MarketPlaceLink.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { OpenSeaIcon } from "../Icons";
 
 const MarketPlaceLinks = ({ id}: { id: string }) => {
-	//id = id + 1;
+	id = id + 1;
 	return (
 		<div className="flex items-center justify-star space-x-1 mt-3 mb-5">
 			{process.env.NEXT_PUBLIC_COLLECTION_TOKEN && (
